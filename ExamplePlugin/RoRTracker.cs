b82f7c1e67c85e8c8fd86c6ecd24215a8997142d
ValeriKozarev <val.kozarev.20@gmail.com>
2026-08-02 15:10:51 -0600

    loading data, but working through NREs in UI hook

--- ExamplePlugin/RoRTracker.cs
+++ ExamplePlugin/RoRTracker.cs
@@ -1,8 +1,14 @@
 using BepInEx;
+using On.RoR2.UI;
 using R2API;
 using RoR2;
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
+using UnityEngine.UI;
+using static Rewired.Utils.Classes.Utility.ObjectInstanceTracker;
+using UnlockableDef = RoR2.UnlockableDef;
 
 namespace RoRTracker
 {
@@ -23,31 +29,108 @@ namespace RoRTracker
         public const string PluginName = "RoRTracker";
         public const string PluginVersion = "0.0.1";
 
-        //The Awake() method is run at the very start when the game is initialized.
+        List<UnlockableDef> pending = new List<UnlockableDef>();
+        UserProfile profile;
+
+        //The Awake() method is run at the very start of the Unity Lifecycle when the game is initialized.
         public void Awake()
         {
             //Init our logging class so that we can properly log for debugging
             Log.Init(Logger);
 
-            // load the local XML file for the player's current progress (C:\Steam\userdata\80402371\632360\remote\UserProfiles, mine is called 62675123-06c8-42f2-bfe1-e42e2f824fe5.xml
-            // and I copied it into /Resources as well
-
-            // load the local XML file that has a completed, 100% unlock for diffing - or alternatively, can we find how the game is tracking incomplete challenges?
-
-            // looks like the UI is doing some separate process to determine if you completed a challenge. The XML only contains corresponding unlock tags if you actually got the unlock already
+            //subscribe our Run_Awake hook to the Run.Awake method from the game, we'll eventually have this load the data we need to display
+            On.RoR2.Run.Awake += Run_Awake;
+            //subscribe our HUD_Awake hook to the HUD.ActivateScoreboard method
+            On.RoR2.UI.HUD.ActivateScoreboard += HUD_ActivateScoreboard;
         }
 
-        // Start() is run after Awake(), and is used to initialize things that require other plugins to be initialized first.
-        public void Start()
+        #region Hooks
+        private void Run_Awake(On.RoR2.Run.orig_Awake orig, Run self)
         {
+            orig.Invoke(self);
 
+            //Makes sure the Update Function is hooked at the start!
+            On.RoR2.Run.Update += Run_Update;
         }
 
-
-        //The Update() method is run on every frame of the game.
-        private void Update()
+        private void Run_Update(On.RoR2.Run.orig_Update orig, Run self)
         {
-            // TODO: Add code here to run every frame, if needed.
+            orig.Invoke(self);
+
+            // try to make sure everything is fully loaded
+            if (RoR2.Run.instance.time < 5)
+                return;
+            
+            profile = LocalUserManager.GetFirstLocalUser().userProfile;
+
+            //iterate over the UnlockableCatalog to figure out what the user is still missing
+            for (UnlockableIndex unlockableIndex = (UnlockableIndex)0; unlockableIndex < (UnlockableIndex)UnlockableCatalog.indexToDefTable.Length; unlockableIndex++)
+            {
+                Log.Info($"Building entry for index {unlockableIndex}");
+                UnlockableDef unlockable = UnlockableCatalog.indexToDefTable[(int)unlockableIndex]; // BepInEx should address these warnings automatically
+                Log.Info($"Building entry for def at index {unlockableIndex}: {unlockable.cachedName}");
+
+                if (!profile.HasUnlockable(unlockable))
+                {
+                    this.pending.Add(unlockable);
+                }
+            }
+
+            Log.Info($"Found {pending.Count} pending unlocks for user {profile.name}.");
+            if (pending.Count > 0)
+            {
+                Log.Info($"First pending unlock is {pending[0].cachedName}");
+            }
+
+            //unsubscribe so we don't try to recompute this list every frame
+            On.RoR2.Run.Update -= Run_Update;
+        }
+
+        private void HUD_ActivateScoreboard(On.RoR2.UI.HUD.orig_ActivateScoreboard orig, RoR2.UI.HUD self)
+        {
+            Log.Info("TOP OF HUD_ACTIVATE_SCOREBOARD");
+            orig.Invoke(self);
+            Log.Info($"HUD_ACTIVATE_SCOREBOARD HOOK: {pending.Count} pending unlocks for user {profile.name}.");
+            this.BuildPendingUnlocksPanel(self);
+        }
+        #endregion
+
+        private void BuildPendingUnlocksPanel(RoR2.UI.HUD hud)
+        {
+            Log.Info("Begin building pending unlocks panel.");
+            GameObject panel = new GameObject("PendingUnlocksPanel");
+            panel.transform.SetParent(hud.scoreboardPanel.transform, false);
+
+            //create the parent RectTransform and set its anchors, pivot, position, and size. our content will go within
+            RectTransform rt = panel.AddComponent<RectTransform>();
+            rt.anchorMin = new Vector2(0f, 0f);
+            rt.anchorMax = new Vector2(1f, 0f);
+            rt.pivot = new Vector2(0.5f, 1f);
+            rt.anchoredPosition = new Vector2(0f, -20f);
+            rt.sizeDelta = new Vector2(0f, 200f);
+
+            //this lets us stack the text entries per unlock vertically
+            VerticalLayoutGroup layout = panel.AddComponent<VerticalLayoutGroup>();
+            layout.childForceExpandHeight = false;
+            layout.spacing = 4f;
+
+            Log.Info("Begin adding in the unlocks to the UI:");
+
+            foreach (UnlockableDef def in pending)
+            {
+                Log.Info($"Adding entry for {def.cachedName ?? "Unlockable"}");
+                GameObject entry = new GameObject(def.cachedName ?? "Unlockable");
+                entry.transform.SetParent(panel.transform, false);
+
+                TextMeshProUGUI text = entry.AddComponent<TextMeshProUGUI>();
+                text.text = Language.GetString(def.nameToken);
+                text.fontSize = 14f;
+                text.color = Color.white;
+            }
         }
     }
 }
+
+
+
+
